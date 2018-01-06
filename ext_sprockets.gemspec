67b1ddfdaeedd0df59634cd955dcf33eb3f35c6a
Patrice Lebel <patleb@users.noreply.github.com>
2018-01-05 21:22:39 -0500

    prepare yarn integration

--- ext_sprockets.gemspec
+++ ext_sprockets.gemspec
@@ -14,16 +14,9 @@ Gem::Specification.new do |s|
   s.description = "ExtSprockets"
   s.license     = "MIT"
 
-  s.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
-  s.test_files = Dir["spec/**/*"]
+  s.files = Dir["{bin,lib}/**/*", "MIT-LICENSE", "README.md"]
 
-  s.add_dependency "rails", "~> 5.0"
+  s.add_dependency "sprockets-rails", "~> 3.2"
 
-  s.add_development_dependency "sqlite3"
-  s.add_development_dependency 'rspec-rails', '~> 3.5'
-  s.add_development_dependency 'fantaskspec', '~> 1.0'
-  s.add_development_dependency 'shoulda-matchers', '~> 3.1'
-  s.add_development_dependency 'shoulda-callback-matchers', '~> 1.1'
-  s.add_development_dependency 'rails-controller-testing'
-  s.add_development_dependency 'email_spec', '~> 2.1'
+  s.add_development_dependency "railties"
 end
--- lib/ext_sprockets/engine.rb
+++ lib/ext_sprockets/engine.rb
@@ -1,30 +1,11 @@
-require "ext_sprockets/configuration"
-
 module ExtSprockets
   class Engine < ::Rails::Engine
-   
-    # Initialize engine dependencies on wrapper application
-    Gem.loaded_specs["ext_sprockets"].dependencies.each do |d|
-      begin
-        require d.name
-      rescue LoadError => e
-        # Put exceptions here.
-      end
+    initializer 'ext_sprockets.assets.paths' do |app|
+      app.config.assets.paths << root.join('node_modules')
     end
-   
-    # Uncomment if migrations need to be shared
-    # initializer :append_migrations do |app|
-    #   unless app.root.to_s.match root.to_s
-    #     config.paths["db/migrate"].expanded.each do |expanded_path|
-    #       app.config.paths["db/migrate"] << expanded_path
-    #     end
-    #   end
-    # end
-   
-    config.to_prepare do
-      # Dir[Engine.root.join("{app,lib}/**/*_decorator.rb")].each do |c|
-      #   require_dependency(c)
-      # end
+
+    initializer 'ext_sprockets.assets.precompile' do |app|
+      app.config.assets.precompile << 'roboto-fontface/fonts/*'
     end
   end
 end
