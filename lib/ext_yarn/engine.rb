d86e26e8241c66496d0077bdeb1ad3851d1d7a33
Patrice Lebel <patleb@users.noreply.github.com>
2018-06-27 13:15:35 -0400

    font-awesome-sass added

--- lib/ext_yarn/engine.rb
+++ lib/ext_yarn/engine.rb
@@ -2,10 +2,16 @@ module ExtYarn
   class Engine < ::Rails::Engine
     initializer 'ext_yarn.assets.paths' do |app|
       app.config.assets.paths << root.join('node_modules')
+
+      %w(fonts stylesheets).each do |type|
+        app.config.assets.paths << root.join('node_modules/font-awesome-sass/assets', type)
+      end
     end
 
     initializer 'ext_yarn.assets.precompile' do |app|
       app.config.assets.precompile << 'roboto-fontface/fonts/*'
+
+      app.config.assets.precompile << "font-awesome/fontawesome-webfont.*"
     end
   end
 end
