e769905df4a2e82741e18e42239cd28fd2de2045
Patrice Lebel <patleb@users.noreply.github.com>
2018-06-27 13:22:15 -0400

    bootstrap-sass assets pipeline path correction

--- lib/ext_yarn/engine.rb
+++ lib/ext_yarn/engine.rb
@@ -3,6 +3,10 @@ module ExtYarn
     initializer 'ext_yarn.assets.paths' do |app|
       app.config.assets.paths << root.join('node_modules')
 
+      %w(javascripts stylesheets).each do |type|
+        app.config.assets.paths << root.join('node_modules/bootstrap-sass/assets', type)
+      end
+
       %w(fonts stylesheets).each do |type|
         app.config.assets.paths << root.join('node_modules/font-awesome-sass/assets', type)
       end
